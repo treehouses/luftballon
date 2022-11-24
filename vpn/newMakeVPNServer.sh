d197089339efa03f58d627ffca9ad9bf71c37095
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2022-11-24 04:58:11 +0000

    generate tls-auth and client inline conf by easy-tls

--- vpn/newMakeVPNServer.sh
+++ vpn/newMakeVPNServer.sh
@@ -1,3 +1,4 @@
+# Make pki, one master ca, one server, and one client
 cd /usr/share/easy-rsa/
 cp vars.example vars
 ./easyrsa init-pki
@@ -6,10 +7,15 @@ cp vars.example vars
 ./easyrsa sign-req server openvpn-server
 ./easyrsa gen-dh
 ./easyrsa gen-req client-1 nopass
-/easyrsa sign-req client client-1
-cd pki/private/
-openvpn --genkey --secret tls-auth.key
+./easyrsa sign-req client client-1
 
+# Make tls-auth.key and make a part of the inline client conf
+# The client-1 contains ca, cert, key, and tls-auth
+./easytls init-tls
+./easytls build-tls-auth
+./easytls inline-tls-auth client-1
+
+# Make server conf file
 cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/server/
 gunzip /etc/openvpn/server/server.conf.gz
 
@@ -20,8 +26,9 @@ sed -i 's/dh2048.pem/\/usr\/share\/easy-rsa\/pki\/dh.pem/' /etc/openvpn/server/s
 sed -i 's/;push "redirect-gateway def1 bypass-dhcp"/push "redirect-gateway def1 bypass-dhcp"/' /etc/openvpn/server/server.conf 
 sed -i 's/;push "dhcp-option DNS 208.67.222.222"/push "dhcp-option DNS 208.67.222.222"/' /etc/openvpn/server/server.conf 
 sed -i 's/;push "dhcp-option DNS 208.67.220.220"/push "dhcp-option DNS 208.67.220.220"/' /etc/openvpn/server/server.conf 
-sed -i 's/tls-auth ta.key 0/tls-auth \/usr\/share\/easy-rsa\/pki\/private\/tls-auth.key 0/' /etc/openvpn/server/server.conf
+sed -i 's/tls-auth ta.key 0/tls-auth \/usr\/share\/easy-rsa\/pki\/easytls\/tls-auth.key 0/' /etc/openvpn/server/server.conf
 
+# Open firewall relating to the default vpn port
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf 
 sysctl -p
 
@@ -33,6 +40,7 @@ ufw allow OpenSSH
 ufw disable
 ufw enable
 
+# Start openvpn-server
 systemctl -f enable openvpn-server@server.service
 systemctl start openvpn-server@server.service
 systemctl status openvpn-server@server.service
