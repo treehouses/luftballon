57f95cc4a44fc44d6d30261e99e0cd718b6dc885
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2022-12-22 03:28:01 +0000

    simplified script

--- vpn/executeScriptOnRemoteServer.sh
+++ vpn/executeScriptOnRemoteServer.sh
@@ -7,15 +7,8 @@ publicIp=$(extractValueFromTreehousesConfig publicIp)
 
 echo $publicIp
 
-#scp installOpenVPVServer.sh root@$instanceId:~/
-#ssh root@$instanceId 'bash installOpenVPVServer.sh'
-
 ssh root@$publicIp '
-    apt update;
-    apt install -y gnupg;
-    curl -fsSL https://swupdate.openvpn.net/repos/repo-public.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/openvpn-repo-public.gpg;
-    echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/openvpn-repo-public.gpg] https://build.openvpn.net/debian/openvpn/stable buster main" > /etc/apt/sources.list.d/openvpn-aptrepo.list;
-    apt-get update && apt-get install -y openvpn ufw unzip'
+    apt update && apt install -y openvpn'
 
 scp /etc/openvpn/server/server.conf root@$publicIp:/etc/openvpn/server/
 
