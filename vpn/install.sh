f4cdb52b7d5bb65db0d07bb31123321f4aaf2291
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2022-12-23 22:26:52 -0600

    install stable openvpn

--- vpn/install.sh
+++ vpn/install.sh
@@ -1,11 +1,7 @@
 # Install openvpn, ufw, upzip, and easyrsa
 # easyrsa is installed with openvpn
 # easyrsa is in /usr/share/easy-rsa/
-apt update
-apt install -y gnupg
-curl -fsSL https://swupdate.openvpn.net/repos/repo-public.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/openvpn-repo-public.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/openvpn-repo-public.gpg] https://build.openvpn.net/debian/openvpn/stable buster main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
-apt-get update && apt-get install -y openvpn ufw unzip
+apt update && apt install -y openvpn unzip
 
 # Install easytls
 cd /usr/share/easy-rsa/
