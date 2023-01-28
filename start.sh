2de1448ad95140e076aed34652799dcfffc77ef0
JLKwong <61807745+JLKwong@users.noreply.github.com>
2023-01-27 21:13:23 -0800

    Update start.sh
    Removing automatic install.

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ publickey=id_rsa.pub
 groupName=luftballons-sg
 instanceName=luftballon
 
-aws --version || ./installAwsCli.sh || echo "Run installAwsCli.sh first. AWS CLI is not installed." && exit 1
+aws --version || echo "Run './installAwsCli.sh' first. AWS CLI is not installed." && exit 1
 
 function importSshKey()
 {
