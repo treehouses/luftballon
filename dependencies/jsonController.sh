f006a3ee1f7ef1382d252a115a339c2d784538dd
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2023-02-01 21:16:44 -0600

    Separate function

--- dependencies/jsonController.sh
+++ dependencies/jsonController.sh
@@ -1,36 +1,40 @@
 
-function addAttribute(){
+function addKeyValue(){
     input="$1"
     name="$2"
     attribute="$3"
     value="$4"
-    isArray="$5"
 
-    if [[ $isArray == 0 ]] 
-    then
-        isNull=$(jq --arg name $name --arg attribute $attribute 'getpath([$name,$attribute])'  <<< "$input")
-        if [ "$isNull" == "null" ] 
-        then 
-            output=$( echo "$input" \
-            | jq --arg name $name \
-            --arg attribute $attribute \
-            --arg value $value \
-            'setpath([$name,$attribute]; [$value])' ) 
-        else 
-            length=$(jq  --arg name $name --arg attribute $attribute 'getpath([$name,$attribute]) | length'  <<< "$input")
-            output=$( echo "$input" \
-            | jq --arg name $name \
-            --arg attribute $attribute \
-            --arg value $value \
-            --argjson length $length \
-            'setpath([$name,$attribute,$length]; $value)' ) 
-        fi
-    else
+    output=$( echo "$input" \
+        | jq --arg name $name \
+        --arg attribute $attribute \
+        --arg value $value \
+        'setpath([$name,$attribute]; $value)' )
+    echo "$output"
+}
+
+function addKeyArray(){
+    input="$1"
+    name="$2"
+    attribute="$3"
+    value="$4"
+    
+    isNull=$(jq --arg name $name --arg attribute $attribute 'getpath([$name,$attribute])'  <<< "$input")
+    if [ "$isNull" == "null" ] 
+    then 
         output=$( echo "$input" \
-            | jq --arg name $name \
-            --arg attribute $attribute \
-            --arg value $value \
-            'setpath([$name,$attribute]; $value)' )
+        | jq --arg name $name \
+        --arg attribute $attribute \
+        --arg value $value \
+        'setpath([$name,$attribute]; [$value])' ) 
+    else 
+        length=$(jq  --arg name $name --arg attribute $attribute 'getpath([$name,$attribute]) | length'  <<< "$input")
+        output=$( echo "$input" \
+        | jq --arg name $name \
+        --arg attribute $attribute \
+        --arg value $value \
+        --argjson length $length \
+        'setpath([$name,$attribute,$length]; $value)' ) 
     fi
     echo "$output"
 }
@@ -45,15 +49,15 @@ function init(){
 
 function testAddAttribute(){
     value=$(init luftballon)
-    value=$(addAttribute "$value" luftballon instanceName luftballon )
-    value=$(addAttribute "$value" luftballon groupName luftballon-sg )
-    value=$(addAttribute "$value" luftballon keyName luftballon )
-    value=$(addAttribute "$value" luftballon publicIp 54.164.69.111 )
-    value=$(addAttribute "$value" luftballon instanceId i-05595f9fe283c9b97 )
-    value=$(addAttribute "$value" luftballon portArray 2200 0 )
-    value=$(addAttribute "$value" luftballon portArray 2222 0 )
-    value=$(addAttribute "$value" luftballon sshtunnelArray 2222:22 0 )
-    value=$(addAttribute "$value" luftballon sshtunnelArray 2233:33 0 )
+    value=$(addKeyValue "$value" luftballon instanceName luftballon )
+    value=$(addKeyValue "$value" luftballon groupName luftballon-sg )
+    value=$(addKeyValue "$value" luftballon keyName luftballon )
+    value=$(addKeyValue "$value" luftballon publicIp 54.164.69.111 )
+    value=$(addKeyValue "$value" luftballon instanceId i-05595f9fe283c9b97 )
+    value=$(addKeyArray "$value" luftballon portArray 2200 )
+    value=$(addKeyArray "$value" luftballon portArray 2222 )
+    value=$(addKeyArray "$value" luftballon sshtunnelArray 2222:22 )
+    value=$(addKeyArray "$value" luftballon sshtunnelArray 2233:33 )
     echo "$value"
 }
 
