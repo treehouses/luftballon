5a15427610e8bca522fae93c4ef76900a788e0cf
dogi <dogi@users.noreply.github.com>
2023-02-24 23:26:14 -0500

    Update executeScriptOnRemoteServer.sh

--- vpn/executeScriptOnRemoteServer.sh
+++ vpn/executeScriptOnRemoteServer.sh
@@ -12,7 +12,6 @@ ssh root@$publicIp '
 
 scp /etc/openvpn/server/server.conf root@$publicIp:/etc/openvpn/server/
 
-
 ssh root@$publicIp " 
     ipAddress=\$(ip a | grep -A 1 eth0 | awk '\$1 == \"inet\" {print \$2}' | sed -n 's/\([0-9\.]\+\).*/\1/p' ); 
     line1=\"push \\\"route \$ipAddress\\\"\"; 
@@ -21,4 +20,3 @@ ssh root@$publicIp "
     systemctl -f enable openvpn-server@server.service;
     systemctl start openvpn-server@server.service;
     systemctl status openvpn-server@server.service"
-
