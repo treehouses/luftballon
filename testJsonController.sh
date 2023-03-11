a84b7b8cb120ed9d0d3a098d28979b923eab0bac
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2023-03-11 01:11:52 -0600

    conduct three test at once by a function

--- testJsonController.sh
+++ testJsonController.sh
@@ -4,11 +4,24 @@ source $manageConfigPath/dependencies/jsonController.sh
 source $manageConfigPath/dependencies/storeConfigByJson.sh
 
 
-instanceName=yourballon
-keyName=yourballon
-instanceId=i-0c4c9b1efb2d505e0
-publicIp=23.91.33.15
-groupName=yourballon-sg
+first_instanceName=firstballon
+first_keyName=firstballon
+first_instanceId=i-0c4c9b1efb2d505e0
+first_publicIp=1.1.1.1
+first_groupName=firstballon-sg
+
+second_instanceName=secondballon
+second_keyName=scondballon
+second_instanceId=i-0c4c9b1efb2d505e0
+second_publicIp=2.2.2.2
+second_groupName=secondballon-sg
+
+third_instanceName=thirdballon
+third_keyName=thirdballon
+third_instanceId=i-0c4c9b1efb2d505e0
+third_publicIp=3.3.3.3
+third_groupName=thirdballon-sg
+
 portArray=2200,1194,22,2222
 sshtunnelArray=2222:22
 configName=testLuftballonConfigs
@@ -27,55 +40,74 @@ function printAllConfig(){
     echo $allConfig
 }
 
-function ifConfigIsEmptyJustMakeConfigAndStore(){
-    allConfig=$(getConfigAsJson $configName)
-	if [ -z $allConfig ]
-	then
-		storeConfig $instanceName $keyName $instanceId $publicIp $groupName 
-	fi
-}
+function storeConfigIntoTreehousesConfigAsStringfiedJson(){
+	instanceName=$1
+	keyName=$2
+	instanceId=$3
+	publicIp=$4
+	groupName=$5
 
-
-function ifKeyExistUpdateTheValue(){
 	local evaluate
     allConfig=$(getConfigAsJson $configName)
 	if [ -z $allConfig ]
 	then
 		storeConfig $instanceName $keyName $instanceId $publicIp $groupName 
+	else
+		evaluate=$(isKey "$allConfig" $instanceName)
 	fi
-    evaluate=$(isKey "$allConfig" $instanceName)
+
     if [ $evaluate == true ]
     then
-        replaceValueAndStoreConfig "$allConfig" $instanceName differentKey $instanceId 128.0.0.1 $groupName 
+        replaceValueAndStoreConfig "$allConfig" $instanceName $keyName $instanceId $publicIp $groupName
+	else if [ $evaluate == false ]
+    then
+        newConfig=$(makeConfig $instanceName $keyName $instanceId $publicIp $groupName)
+        merge=$(merge "$allConfig" "$newConfig")
+        string=$(stringfy "$merge")
+        treehouses config add $configName $string 
     fi
+}
+
+function ifConfigIsEmptyJustMakeConfigAndStore(){
+	instanceName=$1
+	keyName=$2
+	instanceId=$3
+	publicIp=$4
+	groupName=$5
+	
+	storeConfigIntoTreehousesConfigAsStringfiedJson $instanceName $keyName $instanceId $publicIp $groupName
+	echo ifConfigIsEmptyJustMakeConfigAndStore
     printAllConfig
 }
 
-
 function ifKeyNotExistUpdateMakeNewBucket(){
-	local evaluate
-    allConfig=$(getConfigAsJson $configName)
-	if [ -z $allConfig ]
-	then
-		storeConfig $instanceName $keyName $instanceId $publicIp $groupName 
-	fi
-    evaluate=$(isKey "$allConfig" luftballon)
-    if [ $evaluate == false ]
-    then
-        newConfig=$(makeConfig differentBallon mygroupBallon $instanceId 12.0.0.0 mygroup)
-        merge=$(merge "$allConfig" "$newConfig")
-        string=$(stringfy "$merge")
-        treehouses config add $configName $string
-    fi
+	instanceName=$1
+	keyName=$2
+	instanceId=$3
+	publicIp=$4
+	groupName=$5
+	
+	storeConfigIntoTreehousesConfigAsStringfiedJson $instanceName $keyName $instanceId $publicIp $groupName
+	echo ifKeyNotExistUpdateMakeNewBucket
+    printAllConfig
+}
+
+function ifKeyExistUpdateTheValue(){
+	instanceName=$1
+	keyName=$2
+	instanceId=$3
+	publicIp=$4
+	groupName=$5
+	
+	storeConfigIntoTreehousesConfigAsStringfiedJson $instanceName $keyName $instanceId $publicIp $groupName
+	echo ifKeyExistUpdateTheValue
     printAllConfig
 }
 
 treehouses config delete $configName 
-ifConfigIsEmptyJustMakeConfigAndStore
-#storeConfig $instanceName $keyName $instanceId $publicIp $groupName 
-printAllConfig
-ifKeyNotExistUpdateMakeNewBucket
-ifKeyExistUpdateTheValue
+ifConfigIsEmptyJustMakeConfigAndStore $first_instanceName $first_keyName $first_instanceId $first_publicIp $first_groupName
+ifKeyNotExistUpdateMakeNewBucket $second_instanceName $second_keyName $second_instanceId $second_publicIp $second_groupName
+ifKeyExistUpdateTheValue $third_instanceName $third_keyName $third_instanceId $third_publicIp $third_groupName
 
 #merge=$(merge "$prev" "$value")
 #string=$(stringfy "$merge")
