ce02fe0e378c52250746091d1f5c9e4d17e49268
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2023-05-31 21:39:01 -0500

    feat: use the new functions

--- start.sh
+++ start.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 manageConfigPath=$(pwd)
-source $manageConfigPath/dependencies/manageConfig.sh
-source $manageConfigPath/dependencies/jsonController.sh
-source $manageConfigPath/dependencies/storeConfigByJson.sh
+
+source $manageConfigPath/dependencies/configFunctions.sh
 source $manageConfigPath/dependencies/config.sh
 source $manageConfigPath/dependencies/isBalloonNameValid.sh
+source $manageConfigPath/dependencies/manageConfig.sh
+source $manageConfigPath/dependencies/securitygroupFunction.sh
+source $manageConfigPath/dependencies/utilitityFunction.sh
+source $manageConfigPath/dependencies/configOperations.sh
+source $manageConfigPath/dependencies/getLatestIpAddress.sh
+source $manageConfigPath/dependencies/jsonOperations.sh
+source $manageConfigPath/dependencies/reverseShell.sh
+source $manageConfigPath/dependencies/sshtunnelFunction.sh
+
 
 #BASE=$HOME
 BASE=/home/pi
--- testStorePort.sh
+++
@@ -1,8 +0,0 @@
-manageConfigPath=$(pwd)
-source $manageConfigPath/dependencies/manageConfig.sh
-source $manageConfigPath/dependencies/jsonController.sh
-
-value=$(getArrayValueAsStringByKey luftballon portArray)
-echo "$value"
-
-#storePortArrayString
