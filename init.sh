6a911ef4b21b7b76ec167fe684aae8c069a3bd36
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2023-05-31 22:14:41 -0500

    fix: fix order of the source files

--- init.sh
+++ init.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 manageConfigPath=$(pwd)
-source $manageConfigPath/dependencies/configFunctions.sh
 source $manageConfigPath/dependencies/config.sh
+source $manageConfigPath/dependencies/utilitiyFunction.sh
 source $manageConfigPath/dependencies/isBalloonNameValid.sh
-source $manageConfigPath/dependencies/manageConfig.sh
-source $manageConfigPath/dependencies/securitygroupFunction.sh
-source $manageConfigPath/dependencies/utilitityFunction.sh
-source $manageConfigPath/dependencies/configOperations.sh
-source $manageConfigPath/dependencies/getLatestIpAddress.sh
 source $manageConfigPath/dependencies/jsonOperations.sh
-source $manageConfigPath/dependencies/reverseShell.sh
+source $manageConfigPath/dependencies/configOperations.sh
+source $manageConfigPath/dependencies/configFunctions.sh
+source $manageConfigPath/dependencies/getLatestIpAddress.sh
+source $manageConfigPath/dependencies/securitygroupFunction.sh
+source $manageConfigPath/dependencies/manageConfig.sh
 source $manageConfigPath/dependencies/sshtunnelFunction.sh
-
+source $manageConfigPath/dependencies/reverseShell.sh
 
 portConfigArray=
 udpPortConfigArray=
