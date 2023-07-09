f084e26166a699440dbfae35728981f4dfb1d70d
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2023-07-08 21:10:42 -0500

    use openvpn

--- vpn/executeScriptOnRemoteServer.sh
+++ vpn/executeScriptOnRemoteServer.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+balloonName=$1
 manageConfigPath=$(pwd)
-source $manageConfigPath/../dependencies/manageConfig.sh
 
-publicIp=$(getTreehousesConfigValue publicIp)
+source $manageConfigPath/../dependencies/config.sh
+source $manageConfigPath/../dependencies/utilitiyFunction.sh
+source $manageConfigPath/../dependencies/isBalloonNameValid.sh
+source $manageConfigPath/../dependencies/jsonOperations.sh
+source $manageConfigPath/../dependencies/configOperations.sh
+source $manageConfigPath/../dependencies/configFunctions.sh
+source $manageConfigPath/../dependencies/getLatestIpAddress.sh
+source $manageConfigPath/../dependencies/securitygroupFunction.sh
+source $manageConfigPath/../dependencies/manageConfig.sh
+source $manageConfigPath/../dependencies/sshtunnelFunction.sh
+source $manageConfigPath/../dependencies/reverseShell.sh
+
+publicIp=$(getValueByAttribute $balloonName publicIp)
 
 echo $publicIp
 
--- vpn/makeVPNClient.sh
+++ vpn/makeVPNClient.sh
@@ -1,12 +1,24 @@
 
+balloonName=$1
 manageConfigPath=$(pwd)
+
+source $manageConfigPath/../dependencies/config.sh
+source $manageConfigPath/../dependencies/utilitiyFunction.sh
+source $manageConfigPath/../dependencies/isBalloonNameValid.sh
+source $manageConfigPath/../dependencies/jsonOperations.sh
+source $manageConfigPath/../dependencies/configOperations.sh
+source $manageConfigPath/../dependencies/configFunctions.sh
+source $manageConfigPath/../dependencies/getLatestIpAddress.sh
+source $manageConfigPath/../dependencies/securitygroupFunction.sh
 source $manageConfigPath/../dependencies/manageConfig.sh
+source $manageConfigPath/../dependencies/sshtunnelFunction.sh
+source $manageConfigPath/../dependencies/reverseShell.sh
+
 source getRunningVPNEntityConfName.sh
 source deleteEasytlsIClientnline.sh
 
 startpath=$(pwd)
-publicIp=$(getTreehousesConfigValue publicIp)
-
+publicIp=$(getValueByAttribute $balloonName publicIp)
 
 function makeClientConf(){
     clientName=$1
