c554e3dace60923f867000e7cde9163992efa104
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2024-01-24 22:29:14 -0600

    try to use delete.sh

--- src/aws/delete.sh
+++ src/aws/delete.sh
@@ -19,46 +19,41 @@ function delete(){
       exit 1
   fi
 
-  #keyName=$(getValueByAttribute $balloonName key)
-  #groupName=$(getValueByAttribute $balloonName groupName)
+  keyName=$(getValueByAttribute $balloonName key)
+  groupName=$(getValueByAttribute $balloonName groupName)
 
-  echo "ballonName: $balloonName"
-  echo "instanceId: $instanceId"
-  echo "configName: $configName"
-  #echo "keyName: $keyName"
-  #echo "groupName: $groupName"
 
-  #storePortArrayString $groupName tcp $balloonName
-  #storePortArrayString $groupName udp $balloonName
-  #updateSshtunnelConfig $balloonName
-#
-  #echo $instanceId
-  #aws ec2 terminate-instances --instance-ids $instanceId 
-  #echo "ec2 instance delete"
+  storePortArrayString $groupName tcp $balloonName
+  storePortArrayString $groupName udp $balloonName
+  updateSshtunnelConfig $balloonName
 
-  #echo $keyName
-  #aws ec2 delete-key-pair --key-name $keyName
-  #echo "security key delete"
-#
-  #treehouses sshtunnel remove all
-  #echo "remove all sshtunnel"
+  echo $instanceId
+  aws ec2 terminate-instances --instance-ids $instanceId 
+  echo "ec2 instance delete"
 
-  #sleep 30
-  #echo $groupName
-  #while true; do
-  #  output=$(aws ec2 delete-security-group --group-name "$groupName" 2>&1)
-  #  if [[ $? -eq 0 ]]; then
-  #    echo "Security group '$groupName' successfully deleted."
-  #    break
-  #  elif [[ $output == *"DependencyViolation"* ]]; then
-  #    echo "Dependency violation. Retrying in 5 seconds..."
-  #    sleep 10
-  #  else
-  #    echo "An error occurred: $output"
-  #    break
-  #  fi
-  #done
+  echo $keyName
+  aws ec2 delete-key-pair --key-name $keyName
+  echo "security key delete"
 
-  #deleteObsoleteKeyValue $balloonName
+  treehouses sshtunnel remove all
+  echo "remove all sshtunnel"
+
+  sleep 30
+  echo $groupName
+  while true; do
+    output=$(aws ec2 delete-security-group --group-name "$groupName" 2>&1)
+    if [[ $? -eq 0 ]]; then
+      echo "Security group '$groupName' successfully deleted."
+      break
+    elif [[ $output == *"DependencyViolation"* ]]; then
+      echo "Dependency violation. Retrying in 5 seconds..."
+      sleep 10
+    else
+      echo "An error occurred: $output"
+      break
+    fi
+  done
+
+  deleteObsoleteKeyValue $balloonName
 
 }
