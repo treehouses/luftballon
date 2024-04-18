61cd4bc7e4250e66c42330d8ac84843b3bf5b99d
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2024-04-17 22:28:48 -0500

    Refactor extractBlocks to format output and adopt camelCase variables
      - Initialized allPortsPairs for concatenated output.
      - Updated loop to format local and remote ports into a single string.
      - Shifted variable naming to camelCase for consistency.

--- array.sh
+++ array.sh
@@ -1,24 +1,23 @@
 
 extractBlocks() {
-    local input_string="$1"
-    local block_pattern='([0-9]+)\ localhost:([0-9]+)'
-    local -a blocks_array
+    local inputString="$1"
+    local blockPattern='([0-9]+)\ localhost:([0-9]+)'
+    local -a blocksArray
+    local allPortsPairs=""
 
     while read -r block; do
-        if [[ $block =~ $block_pattern ]]; then
-            blocks_array+=("$block")
+        if [[ $block =~ $blockPattern ]]; then
+            blocksArray+=("$block")
         fi
-    done <<< "$(echo $input_string | grep -oE '([0-9]+)\ localhost:[0-9]+')"
+    done <<< "$(echo $inputString | grep -oE '([0-9]+)\ localhost:[0-9]+')"
 
-    for block in "${blocks_array[@]}"; do
-        echo "$block"
+    for block in "${blocksArray[@]}"; do
+        local localPort=$(echo "$block" | cut -d' ' -f1)
+        local remoteHostAndPort=$(echo "$block" | cut -d' ' -f2)
+        local remotePort=$(echo "$remoteHostAndPort" | cut -d':' -f2)
+        allPortsPairs+="${localPort}:${remotePort},"
     done
+    allPortsPairs="${allPortsPairs%,}"
+    echo "$allPortsPairs"
 }
 
-input_string="12345 localhost:22 54321 localhost:80 ..."
-# Capture the output in an array
-readarray -t captured_blocks < <(extractBlocks "$input_string")
-# Print each captured block
-for block in "${captured_blocks[@]}"; do
-    echo "$block"
-done
