146d55e070bb37a622479dcd659b3e73e82e8dd3
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2024-10-12 03:32:20 +0100

    remove tests

--- src/aws/dependencies/detectIncompleteState.sh
+++ src/aws/dependencies/detectIncompleteState.sh
@@ -116,20 +116,7 @@ detectIncompleteState() {
   # Bit 0 -> keyPairExists
   local binaryState=$(((instanceExists << 2) | (securityGroupExists << 1) | keyPairExists))
 
-  echo "Binary state (in binary): $(echo "obase=2; $binaryState" | bc)"
+  #echo "Binary state (in binary): $(echo "obase=2; $binaryState" | bc)"
   return $binaryState
 }
 
-detectIncompleteState "luftballon" "luftballons-sg" "luftballon"
-binaryState=$?
-
-# Analyze each bit
-if instanceExists $binaryState; then
-  echo "Instance exists."
-fi
-if securityGroupExists $binaryState; then
-  echo "Security Group exists."
-fi
-if keyPairExists $binaryState; then
-  echo "Key Pair exists."
-fi
